719c267e44e006fd62ed46ca501811bd490657f5
Christopher Gatherer <chris.gatherer@gmail.com>
2018-12-13 11:41:00 -0500

    Finished automated test scripts

--- app/components/Clock.jsx
+++ app/components/Clock.jsx
@@ -1,10 +1,24 @@
 var React = require('react');
 
 var Clock = React.createClass({
+  formatSeconds: function (totalSeconds) {
+    var seconds = totalSeconds % 60;
+    var minutes = Math.floor(totalSeconds / 60);
+
+    if (seconds < 10) {
+      seconds = '0' + seconds;
+    }
+
+    if (minutes < 10) {
+      minutes = '0' + minutes;
+    }
+
+    return minutes + ':' + seconds;
+  },
   render: function () {
     return (
       <div>
-        
+
       </div>
     );
   }
--- app/tests/components/Clocks.test.jsx
+++ app/tests/components/Clocks.test.jsx
@@ -7,13 +7,27 @@ var TestUtils = require('react-addons-test-utils');
 var Clock = require('Clock');
 
 describe('Clock', () => {
-  formatSeconds: function (totalSeconds) {
-    var seconds = totalSeconds % 60;
-    var minutes = Math.floor(totalSeconds / 60);
-
-    return minutes + ':' + seconds;
-  },
   it('should exist', () => {
     expect(Clock).toExist();
+  });
+
+  describe('formatSeconds', () => {
+    it('should format seconds', () => {
+      var clock = TestUtils.renderIntoDocument(<Clock/>);
+      var seconds = 615;
+      var expected = '10:15';
+      var actual = clock.formatSeconds(seconds);
+
+      expect(actual).toBe(expected);
+    });
+
+    it('should format seconds when min/sec are less then 10', () => {
+      var clock = TestUtils.renderIntoDocument(<Clock/>);
+      var seconds = 61;
+      var expected = '01:01';
+      var actual = clock.formatSeconds(seconds);
+
+      expect(actual).toBe(expected);
+    });
   });
 });
